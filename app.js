e55af52a36fc44e4fcf92c92e47434b4ae6599a6
Heber Samuel Lee <hsl2113@columbia.edu>
2015-12-21 21:55:50 -0500

    3

--- app.js
+++ app.js
@@ -56,8 +56,8 @@ io.on('connection', function(socket) {
         	sessionID: data.s,
         	userName : data.u
         });
-        //console.log('clientSockets.ip = '+clientSockets.ip);
-        authenticate(data.u, data.p, data.s, socket, clientSockets.ip);
+        console.log('clientSockets.ip = '+clientSockets[ip].handshake.address);
+        authenticate(data.u, data.p, data.s, socket, clientSockets[ip]);
     });
 
     socket.on('ios', function (data) {
