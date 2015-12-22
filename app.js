9b0214e9bf75503ce03ae0862e56e4335fd366f7
Heber Samuel Lee <hsl2113@columbia.edu>
2015-12-22 18:11:15 -0500

    websocket fix

--- app.js
+++ app.js
@@ -90,7 +90,9 @@ io.on('connection', function(socket) {
     });
 
     socket.on('receiveResume', function(data) {
-        console.log('received from '+ data.id+ ' url: '+ data.url)
+        console.log('received from '+ data.id+ ' url: '+ data.url);
+        console.log(ip);
+        console.log(webClientSockets(ip));
         webClientSockets[ip].emit('newResume', {data: data.url});
         console.log('sent new resume to client');
     });
