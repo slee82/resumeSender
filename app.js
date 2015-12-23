c885e358bc7a7732e9ebc9e2049dae1dcd86f625
aa920502 <davidlucs@gmail.com>
2015-12-22 22:39:30 -0500

    12

--- app.js
+++ app.js
@@ -262,6 +262,45 @@ function findAccountType(userId){
             });
         }
     });
+
+
 }
 
+
+
+function getUserFile(uid){
+    var S3Bucket = require( "ee-aws-s3-bucket" );
+
+    var myBucket = new S3Bucket( {
+          key:      process.env.aws_access_key_id
+        , secret:   process.env.aws_secret_access_key
+        , bucket:   "cloud2015project"
+        , maxConcurrent: 10                 // generic limit, overrrides the default of 10
+        , maxConcurrentDownloads: 100       // limit to 100 concurrent downloads, overrides the generic limit
+        , maxConcurrentUploads: 50          // limit to 50 concurrent uploads, overrides the generic limit
+        , maxConcurrentDeletes: 200         // limit to 200 concurrent deletes, overrides the generic limit
+        , maxConcurrentLists: 5             // limit to 5 concurrent lists, overrides the generic limit
+    } );
+
+    
+    var handleListResult = function( err, list, next ){
+        if ( err ) log.trace( err );
+        else {
+            var fileList = []
+
+            // console.log( list );
+            for(var i=0;i<list.length; i++){
+                if((list[i].key).indexOf(uid) > -1){
+                    fileList.push('https://s3.amazonaws.com/cloud2015project/'+list[i].key);
+                }
+            }
+            // get the next 1'000 items if available
+            if( next ) next( handleListResult );
+            console.log(fileList);
+        }
+    };
+
+    myBucket.list( 'r' , handleListResult );
+    
+}
 
