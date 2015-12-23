a96f39e1155718434f6f43f844beda5b2bc99d46
Heber Samuel Lee <hsl2113@columbia.edu>
2015-12-22 22:48:01 -0500

    app changes

--- app.js
+++ app.js
@@ -218,6 +218,7 @@ function authenticate(user_id, password, sessionID, socket, iosSocket){
                                 // else if(item.type === 'Prospective Employee'){
                                 //     socket.emit('reply', {url: 'resumeMain/index_employee.html', type: item.type, userId: user_id} )
                                 // }
+                                //getUserFile(user_id, item.type, socket);
                                 socket.emit('reply', {url: 'resumeMain/index.html', type: item.type, userId: user_id} )
                             });
                         }
@@ -268,39 +269,5 @@ function findAccountType(userId){
 
 
 
-function getUserFile(uid){
-    var S3Bucket = require( "ee-aws-s3-bucket" );
 
-    var myBucket = new S3Bucket( {
-          key:      process.env.aws_access_key_id
-        , secret:   process.env.aws_secret_access_key
-        , bucket:   "cloud2015project"
-        , maxConcurrent: 10                 // generic limit, overrrides the default of 10
-        , maxConcurrentDownloads: 100       // limit to 100 concurrent downloads, overrides the generic limit
-        , maxConcurrentUploads: 50          // limit to 50 concurrent uploads, overrides the generic limit
-        , maxConcurrentDeletes: 200         // limit to 200 concurrent deletes, overrides the generic limit
-        , maxConcurrentLists: 5             // limit to 5 concurrent lists, overrides the generic limit
-    } );
-
-    
-    var handleListResult = function( err, list, next ){
-        if ( err ) log.trace( err );
-        else {
-            var fileList = []
-
-            // console.log( list );
-            for(var i=0;i<list.length; i++){
-                if((list[i].key).indexOf(uid) > -1){
-                    fileList.push('https://s3.amazonaws.com/cloud2015project/'+list[i].key);
-                }
-            }
-            // get the next 1'000 items if available
-            if( next ) next( handleListResult );
-            console.log(fileList);
-        }
-    };
-
-    myBucket.list( 'r' , handleListResult );
-    
-}
 
